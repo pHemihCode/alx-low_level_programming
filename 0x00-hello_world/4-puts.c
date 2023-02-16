7c886102d55315bae95990907094b25e1994b2de
pHemihCode <akintanfemi100@gmail.com>
2023-02-16 16:01:22 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
  *Returns 0
  */
-int main()
+int main(void)
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
   
   return 0;
