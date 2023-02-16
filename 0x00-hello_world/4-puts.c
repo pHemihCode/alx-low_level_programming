39275807eed3df169be97d65de0c00c7caffa6a8
pHemihCode <akintanfemi100@gmail.com>
2023-02-16 15:20:34 +0100

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    char sentence["Programming is like building a multilingual puzzle"];
-    
-    puts(sentence);
+    puts("Programming is like building a multilingual puzzle\n");
     
     return 0;
 }
