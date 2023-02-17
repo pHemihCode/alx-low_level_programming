4fb32f483fa3075ae43ddb5ae615be9093863a25
pHemihCode <akintanfemi100@gmail.com>
2023-02-17 20:38:44 +0100

    BaseSixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Result: Always 0 (Success)
+*/
+int main(void)
+{
+int the_num;
+char alpha;
+for(the_num = '0' ; the_num <= '9' ; the_num++)
+putchar(the_num);
+for(alpha = 'a'; alpha <= 'f'; alpha++)
+putchar(alpha);
+putchar('\n');
+return (0);
+}
