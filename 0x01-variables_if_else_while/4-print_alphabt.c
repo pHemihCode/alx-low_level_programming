12d6b357d3de093bb8b9b79ecb06d46df42a7bcb
pHemihCode <akintanfemi100@gmail.com>
2023-02-17 16:01:01 +0100

    Skipp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if(ch == 'e')
+{
+continue;
+}
+else if(ch == 'q')
+{
+continue;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
