bf2af8e2c333be56b239397adc402c5e271863bc
pHemihCode <akintanfemi100@gmail.com>
2023-02-17 14:02:39 +0100

    Else ifs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n,last_digit;
+int n, last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -18,15 +18,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 if(last_digit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n",n,last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if(last_digit == 0)
 {
-printf("Last digit of %d is %d and is 0\n",n,last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else if(last_digit < 6 && !0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n",n,last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 else{
 printf("This must be negative number\n");
