6d6a2742d7b767f82ca665dca59ef20ca2b69416
pHemihCode <akintanfemi100@gmail.com>
2023-02-17 15:30:12 +0100

    Upper with lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,14 @@
 *
 *Return: Always 0 (Success)
 */
-int main() {
+int main(void)
+{
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 putchar(ch);
-for (ch = 'A' ; ch<= 'Z' ; ch++)
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 putchar(ch);
 putchar('\n');
 
-  return (0);
+return (0);
 }
