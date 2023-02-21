9dcc82a9385848cb4a3fbce54f7d8638b90272f7
pHemihCode <akintanfemi100@gmail.com>
2023-02-22 00:36:03 +0100

    Timetable

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_putchar - writes the chracter c to stdout
+*@c: The character to print
+*Return: On success 1.
+*On error,-1 is returned and error is not appropriate
+*/
+int _putchar(char c)
+{
+return (write(1,&c,1));
+}
