ef447e0cb65b201f6a064616bcb36954e73861cd
pHemihCode <akintanfemi100@gmail.com>
2023-02-21 22:02:09 +0100

    Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-*_islower: Check the code
-*
+*_islower: Check for lower case character
+*@c: letter being tested
 *Return: Always 0 (Success)
 */
 int _islower(int c)
 {
-if (c >= 97 && c <=122)
+if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
