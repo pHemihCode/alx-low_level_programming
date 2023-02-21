920f6a157e1ee6cc6382cf39134f7b82084e2aaf
pHemihCode <akintanfemi100@gmail.com>
2023-02-21 22:42:45 +0100

    Absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - returns absolute value of an int
+*@n: integer from whicj to get an absolute value
+*Return: void
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
