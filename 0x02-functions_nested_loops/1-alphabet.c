93f7b1b737b1d11785de9eb0da20f6bd7a93b74e
pHemihCode <akintanfemi100@gmail.com>
2023-02-21 18:22:05 +0100

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+ _putchar('\n');
+}
