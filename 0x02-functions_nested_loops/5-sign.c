5462d9e6e47febe912a42f19d20ad2350327d118
pHemihCode <akintanfemi100@gmail.com>
2023-02-21 22:33:35 +0100

    Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_sign - Check the code 
+*@n: is n an alphabet
+*
+*Return: Always 0 (Success)
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
