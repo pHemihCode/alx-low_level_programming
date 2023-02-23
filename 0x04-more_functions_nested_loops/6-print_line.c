76a38c2b9518d7b7c7760bef98ff1a7ae6d21509
pHemihCode <akintanfemi100@gmail.com>
2023-02-23 20:10:42 +0100

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_line - Print line
+*@n: Variable
+*Return: Void
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
