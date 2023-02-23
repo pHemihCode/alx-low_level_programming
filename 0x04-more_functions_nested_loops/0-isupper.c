642bc97cabdde74f8a63c200dd6a72e35f518390
pHemihCode <akintanfemi100@gmail.com>
2023-02-23 16:10:27 +0100

    Upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_isupper - Initiating function
+*@c: variable of upper or lower case 
+*
+*Return: Always 0
+*/
+
+int _isupper(int c)
+{
+if (c <= 65 && c >= 90)
+{
+return (1);
+}
+return (0);
+}
