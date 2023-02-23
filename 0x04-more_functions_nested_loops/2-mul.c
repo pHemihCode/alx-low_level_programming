3c9f057f847b41e355ec1096a43bd6f00bb13999
pHemihCode <akintanfemi100@gmail.com>
2023-02-23 16:31:36 +0100

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*mul - Multiplication function initiator
+*@a: First parameter
+*@b: Second parameter;
+*Return: Always 0
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
