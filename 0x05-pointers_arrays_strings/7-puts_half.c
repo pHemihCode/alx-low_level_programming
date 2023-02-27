a18b1d962b13fe77d45bda521b4fd350d50a4b49
pHemihCode <akintanfemi100@gmail.com>
2023-02-28 00:18:23 +0100

    Second halves

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string.
+ *@str: string
+ */
+
+void puts_half(char *str)
+{
+int length, n, i;
+for (length = 0; str[length] != '\0'; length++)
+{
+}
+n = (length - 1) / 2;
+for (i = n + 1; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
