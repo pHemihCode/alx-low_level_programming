d9ef1bb045e27e583389ddfd35756755c648f64a
pHemihCode <akintanfemi100@gmail.com>
2023-02-28 00:41:01 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ **_strcpy - Copies the string
+ *@src: pointer
+ *@dest: pointer
+ *Return: return pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i, n;
+for (i = 0; src[i] != '\0'; i++);
+for (n = 0; n <= i; n++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
