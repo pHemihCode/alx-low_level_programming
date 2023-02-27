cbc8d210e77ce2fd960ea05850f80443b0556849
pHemihCode <akintanfemi100@gmail.com>
2023-02-27 22:43:47 +0100

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - Function that swaps the numbers
+ *@c: The third number
+ *Return: Alwways o
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
