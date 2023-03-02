3df9ea379de81003656e7e9eb970bb7bd8d2bbf7
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 23:35:11 +0100

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rot13 - Encodes a string using rot13.
  * @s: An input string 
@@ -10,11 +9,9 @@ char *rot13(char *s)
 int i = 0;
 while (s[i] != '\0')
 {
-while ((s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= 'A' && s[i] <= 'Z'))
+while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 {
-if ((s[i] >= 'a' && s[i] <= 'm') ||
-(s[i] >= 'A' && s[i] <= 'M'))
+if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 s[i] += 13;
 else
 s[i] -= 13;
