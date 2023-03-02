2bef56f4b313dcf9ca966d1a2efeba190924858c
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 21:52:24 +0100

    Comparing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int flag = 0;
-int i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+int result = 0;
+while (*s1 == *s2 && *s1 != '\0')
 {
-if (s1[i] != s2[i])
-{
-flag = 1;
-break;
+s1++;
+s2++;
 }
-i++;
-}
-if (flag == 0)
-{
-return (0);
-}
-else
-{
-return (1);
-}
+if (s1 != s2)
+result = *s1 - *s2;
+return (result);
 }
