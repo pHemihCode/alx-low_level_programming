c9a528385fa1eb41b1222d1e4335f0e28f44fd1d
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 21:15:29 +0100

    Copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strncpy - Function that copies
+ *@dest: destination string
+ *@src: source string
+ *@n: number
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
