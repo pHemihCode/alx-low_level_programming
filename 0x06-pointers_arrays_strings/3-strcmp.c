3180c3001473d78ac5cc74ceea36f743031297c7
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 21:37:54 +0100

    Comparing

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - Compares strings
+ *@s1: destination string
+ *@s2: Source string
+ *Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int flag = 0;
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+flag = 1;
+break;
+}
+i++;
+}
+if (flag == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
