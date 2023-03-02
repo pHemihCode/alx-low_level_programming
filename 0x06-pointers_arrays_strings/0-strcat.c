0727a8889b77ebc04f86ee1741c9d640d9eef345
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 13:30:06 +0100

    Concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,17 @@
 char *_strcat(char *dest, char *src)
 {
 int i,j;
-for (i = 0; dest[i] != '\0'; i++)
+i = 0;
+while (dest[i] != '\0')
 {
+i++;
 }
-for (j = 0; src[j] != '\0'; j++,i++)
+j = 0;
+while (src[j] != '\0')
 {
 dest[i] = src[j];
+i++;
+j++;
 }
 dest[i] = '\0';
 }
