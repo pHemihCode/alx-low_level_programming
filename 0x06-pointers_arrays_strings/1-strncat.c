2f47c844654af932f2c5fa5bc3ed7041db01f59b
pHemihCode <akintanfemi100@gmail.com>
2023-03-02 20:21:40 +0100

    Concatenates with n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcncat - Function that concatenates
+ *@src: Source string
+ *@dest: Destination string
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = strlen(dest);
+int j = 0;
+while (j < n && *src)
+{
+dest[i + j] = *src;
+src++;
+j++;
+}
+dest[i + j] = '\0';
+return (dest);
+}
