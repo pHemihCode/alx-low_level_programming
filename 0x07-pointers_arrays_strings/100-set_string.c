84ce06d69d6847ae62552e209188637d45803406
pHemihCode <akintanfemi100@gmail.com>
2023-03-06 21:32:29 +0100

    Set strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *set_string - A function that sets the pointer
+ *@s: The pointer having another pointer address as value
+ *@to: A pointer
+ *Return: Void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
