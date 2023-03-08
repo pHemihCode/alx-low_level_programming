b3da6fd2d5a5a44fd991d6c3324cac2c07bc06b6
pHemihCode <akintanfemi100@gmail.com>
2023-03-08 10:56:57 +0100

    Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,11 +4,15 @@
  *@n: The number
  *Return: The factorial of n
  */
-signed int factorial(signed int n)
+int factorial(int n)
 {
 if (n < 0)
 {
 return (-1);
+}
+if (n <= 1)
+{
+return (1);
 }
 return (n * factorial(n - 1));
 }
