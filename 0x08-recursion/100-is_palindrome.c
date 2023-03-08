5615f96919363717987a1c0ded32acaa3030f552
pHemihCode <akintanfemi100@gmail.com>
2023-03-09 00:13:45 +0100

    Palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,10 +4,13 @@
  *@s: The string
  *Return: An integer
  */
+int check(char *s, int i, int len);
+int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 if (*s == 0)
 {
 return (1);
 }
+return (check(s,0,_strlen_recursion(s)));
 }
