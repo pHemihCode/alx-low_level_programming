805a24050ef0a69c8e22ebc62bb6417cbfeb64d0
pHemihCode <akintanfemi100@gmail.com>
2023-03-08 23:25:14 +0100

    Square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - The function 
+ *@n; Integer
+ *Return: Integer
+ */
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+ *_sqrt - _sqrt_recursion
+ *@n: Integer
+ *@i: Integer
+ *Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+if (n < 0)
+{
+return (-1);
+}
+{
+if ((i * i) > n)
+return (-1);
+}
+if (i * i == n)
+{
+return (i);
+}
+return (_sqrt(n, i + 1));
+}
