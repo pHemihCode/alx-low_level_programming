10c2b2b5f188e6d9c66f32b4e3a607a4dc682ff0
pHemihCode <akintanfemi100@gmail.com>
2023-03-12 20:09:41 +0100

    Argv print commands

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
  *main - The entry point
  *@argc: The argument count
  *@argv: The argument vector
- *Return: void
+ *Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-printf("%s\n",argv[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
