3143f066dff8ed5b0e96dfc847c8828b372eb5aa
pHemihCode <akintanfemi100@gmail.com>
2023-03-12 20:14:20 +0100

    Number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - The entry point
+ *@argc: The argument count
+ *@argv: The argument array
+ *Return: always positive
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
