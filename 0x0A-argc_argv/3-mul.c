c891f8d01a352ee24ce44f6b9cd4f21ac8312b96
pHemihCode <akintanfemi100@gmail.com>
2023-03-14 00:24:13 +0100

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_atoi - convert a string into an integer.
- *@s: the string to use.
- *Return: integer.
+ *_atoi - convert a string into an integer
+ *@s: the string to use
+ *Return: integer
  */
 int _atoi(char *s)
 {
