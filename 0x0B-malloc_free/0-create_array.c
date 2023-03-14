e1882a3ddf567bed644edbe3e7226d35a6b5b4e5
femi <akintanfemi100@gmail.com>
2023-03-14 18:11:29 +0100

    Malloc first task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ **create_array - the function that creates array
+ *@size: Size of the array
+ *@c: the character
+ *Return: Null or pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+  unsigned int i;
+  if (size == 0)
+    {
+      return (NULL);
+    }
+  int *ptr = malloc(size * sizeof(c));
+  if (ptr == NULL)
+    return (NULL);
+  
+      for (i = 0; i < size; i++)
+	{
+	  ptr[i] = c;
+	}
+      return (ptr);
+}
+ 
