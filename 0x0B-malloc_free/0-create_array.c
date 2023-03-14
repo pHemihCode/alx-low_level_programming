c96d42fd02284b4149babd3796110df13f611ea3
pHemihCode <akintanfemi100@gmail.com>
2023-03-14 21:39:51 +0100

    Malloc first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  if (size == 0)
-    {
-      return (NULL);
-    }
-  int *ptr = malloc(size * sizeof(c));
-  if (ptr == NULL)
-    return (NULL);
-  
-      for (i = 0; i < size; i++)
-	{
-	  ptr[i] = c;
-	}
-      return (ptr);
+unsigned int i;
+char *ptr;
+if (size == 0)
+{
+return (NULL);
+}
+ptr = malloc(size * sizeof(c));
+if (ptr == NULL)
+return (NULL); 
+for (i = 0; i < size; i++)
+{
+ptr[i] = c;
+}
+return (ptr);
 }
  
