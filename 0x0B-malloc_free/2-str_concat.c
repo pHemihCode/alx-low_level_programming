a76b977f06cc2e351df833dd136419fb2b5114fa
pHemihCode <akintanfemi100@gmail.com>
2023-03-15 18:58:52 +0100

    Malloc third task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,15 @@
 char *str_concat(char *s1, char *s2)
 {
 char *ptr;
- int len1, len2;
+int len1, len2;
 if (s1 == NULL)
 return ("");
 if (s2 == NULL)
 return ("");
 len1 = strlen(s1);
 len2 = strlen(s2);
+ptr = malloc(sizeof(char) * (len1 +len2));
 strcat(s1, s2);
-ptr = malloc(sizeof(char) * (len1 +len2)); 
 if (ptr == NULL)
 {
 return (NULL);
