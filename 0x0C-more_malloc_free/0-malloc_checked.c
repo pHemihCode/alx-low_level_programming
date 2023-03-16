9b228e9d2330b62322864f0e506f79630d187dff
pHemihCode <akintanfemi100@gmail.com>
2023-03-16 20:56:31 +0100

    More on mallocs

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,13 @@
 /**
  *malloc_checked - The function that allocates memory
  *@b: The number of intergers
- *Return: void
+ *Return: Pointer
  */
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
 ptr = malloc(b);
 if (ptr == NULL)
-exit (98);
+exit(98);
 return (ptr);
 }
