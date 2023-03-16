14e74588033e5f95f6e902540659b15cf7edf520
pHemihCode <akintanfemi100@gmail.com>
2023-03-16 23:41:03 +0100

    More on malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,8 +14,12 @@ if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
+{
 return (NULL);
+}
 for (i = 0; i < (nmemb * size); i++)
+{
 *((char *)(ptr) + 1) = 0;
+}
 return (ptr);
 }
