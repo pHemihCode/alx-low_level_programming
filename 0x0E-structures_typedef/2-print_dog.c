054745bf3feef4cf5ef3f0b5db7ddf88ce2bd45f
pHemihCode <akintanfemi100@gmail.com>
2023-03-21 07:04:11 +0100

    Header files created

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,17 +3,14 @@
 /**
  *print_dog - the function that prints member of the struct
  *@d: poiner
- *Return: void
  */
 void print_dog(struct dog *d)
 {
-if (d -> name == NULL)
-{
-d -> name = "(nil)";
-}
-if (d -> owner == NULL)
-d -> owner = "(nil)";
 if (d == NULL)
 return;
+if (d -> name == NULL)
+d -> name = "(nil)";
+if (d -> owner == NULL)
+d -> owner = "(nil)";
 printf("%s\n %s\n",d -> name, d -> owner);
 }
