cec81940dc71c70a22d1ee7cd57bc41d9323f2c5
pHemihCode <akintanfemi100@gmail.com>
2023-03-21 07:15:01 +0100

    Header files created

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,5 +12,5 @@ if (d -> name == NULL)
 d -> name = "(nil)";
 if (d -> owner == NULL)
 d -> owner = "(nil)";
-printf("%s\n %s\n", d -> name, d -> owner);
+printf("Name: %s\n Owner: %s\n", d -> name, d -> owner);
 }
