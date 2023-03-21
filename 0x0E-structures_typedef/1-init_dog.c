eb3f8df0bda70ffef19362ccf611c760af114d4d
pHemihCode <akintanfemi100@gmail.com>
2023-03-21 05:59:20 +0100

    Header files created

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - the function
+ * @d: pointer to struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+d = malloc(sizeof(struct dog));
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ *struct dog - the basic name
+ *@name: member
+ *@age: Second member
+ *@owner: Third member
+ */
 struct dog
 {
   char *name;
   float age;
   char *owner;
 };
+ /**
+ *dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
