f406913e74a5ac92f2892f379fc713c29bdef1de
pHemihCode <akintanfemi100@gmail.com>
2023-03-22 18:48:16 +0100

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,24 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *myname - the name of the function
  *print_name - function that calls another function
- *@tmp: temporary storage
+ *@f: pointer
+ *@name: the string
  *Return: void
  */
-void myname(char *);
-void myname(char *name)
-{
-char *tmp;
-tmp= name;
-while (*tmp != '\0')
-{
-printf("%c", *tmp);
-tmp++;
-}
-}
-
 void print_name(char *name, void (*f)(char *))
 {
+if (f != NULL)
 f(name);
 }
