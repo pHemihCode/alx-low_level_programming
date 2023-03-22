0c5c9804f35d56d35afb30dff5077a21cad95028
pHemihCode <akintanfemi100@gmail.com>
2023-03-22 04:35:54 +0100

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,8 +8,7 @@
  *Return: Null
  */
 /**
- *_strlen - returns length of
- *a string
+ *_strlen - returns length of string
  *@str: string to be counted
  *Return: returns length of string
  */
@@ -23,8 +22,7 @@ return (len);
 }
 
 /**
- *_strcopy - copy string pointed by src
- *into dest variable
+ *_strcopy - copy string pointed by src into dest variable
  *@dest:buffer storing string copy
  *@src: buffer storing string to copy
  *Return:returns copied string
@@ -49,23 +47,23 @@ return (NULL);
 doggo = malloc(sizeof(dog_t));
 if (doggo == NULL)
 return (NULL);
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
+doggo -> name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo -> name == NULL)
 {
 free(doggo);
 return (NULL);
 }
 
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (doggo->owner == NULL)
+doggo -> owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo -> owner == NULL)
 {
-free(doggo->name);
+free(doggo -> name);
 free(doggo);
 return (NULL);
 }
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
+doggo -> name = _strcopy(doggo -> name, name);
+doggo -> age = age;
+doggo -> owner = _strcopy(doggo -> owner, owner);
 
 return (doggo);
 }
