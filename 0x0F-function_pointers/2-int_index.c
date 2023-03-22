8321f4e51a101a2d3b65be071647cb4f9dbd1a17
pHemihCode <akintanfemi100@gmail.com>
2023-03-22 20:23:52 +0100

    Function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ *int_index - the function that calls another function
+ *@array: the array
+ *@size: the size of the array
+ *@cmp: pointer to the function
+ *Return: -1, 0, 1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 1)
+return (i);
+}
+return (1);
+}
