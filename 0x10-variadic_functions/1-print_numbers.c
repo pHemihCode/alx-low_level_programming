b0fb20e87c395850a83a4398b324c231363a0bd3
pHemihCode <akintanfemi100@gmail.com>
2023-03-24 21:03:47 +0100

    Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - print the numbers
+ *@separator: string
+ *@n: number of elements
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+
+if (separator != NULL && i != (n - 1))
+{
+printf("%s", separator);
+}
+}
+putchar('\n');
+va_end(ap);
+}
