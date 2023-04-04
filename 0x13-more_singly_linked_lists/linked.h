7b2774dcb7130383dc2f6df5809f4b9a2a4d8ff7
pHemihCode <akintanfemi100@gmail.com>
2023-04-04 15:24:04 +0100

    Singly linked list

--- 0x13-more_singly_linked_lists/linked.h
+++ 0x13-more_singly_linked_lists/linked.h
@@ -1,5 +1,14 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
+#include <stdio.h>
+#include <string.h>
+
+typedef struct listint_s
+{
+int n;
+struct listint_s *link;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -15,4 +24,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
