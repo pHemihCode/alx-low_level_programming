e56add2b2b34f7ebd8e386d5740fda5e3bfeb663
pHemihCode <akintanfemi100@gmail.com>
2023-04-05 15:05:13 +0100

    Singly linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
-#include <stdio.h>
-#include <string.h>
+
 #include <stddef.h>
 typedef struct listint_s
 {
@@ -24,5 +23,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
+int _putchar(char c);
 #endif
